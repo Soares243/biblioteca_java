6a55f34bc07dca3a1e3c253d8900d23b83ed8b0e
Luiz <lamasluizmiguel@gmail.com>
2026-04-08 16:49:45 -0300

    ada
    adsad

--- biblioteca/src/main/java/br/unisales/database/table/Usuario.java
+++ biblioteca/src/main/java/br/unisales/database/table/Usuario.java
@@ -29,7 +29,7 @@ public class Usuario {
     @Default
     @Enumerated(EnumType.STRING)
     @Column(name = "tipo", nullable = false, length = 20)
-    private UsuarioTipoEnum tipo = UsuarioTipoEnum.ALUNO;
+    private UsuarioTipoEnum tipo = 
 
     @Column(nullable = false, length = 150)
     private String email;
